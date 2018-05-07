5fd8f14c696e7c9c92117d4f9735558bd447212f
Troy Kisky <troy.kisky@boundarydevices.com>
2018-05-07 14:32:40 -0700

    pci-imx6: allow external clock for imx8M
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -90,6 +90,8 @@ struct imx6_pcie {
 	struct clk		*pcie_inbound_axi;
 	struct clk		*pcie;
 	struct clk		*pcie_aux;
+	u32			ext_src_clk_enabled;
+	struct clk		*pcie_ext_src;
 	struct regmap		*iomuxc_gpr;
 	u32			controller_id;
 	struct reset_control	*pciephy_reset;
@@ -354,6 +356,16 @@ static int pcie_phy_write(struct imx6_pcie *imx6_pcie, int addr, u16 data)
 
 static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 {
+	if (imx6_pcie->pcie_ext_src) {
+		int ret;
+
+		imx6_pcie->ext_src_clk_enabled = 1;
+		ret = clk_prepare_enable(imx6_pcie->pcie_ext_src);
+		if (ret)
+			dev_err(imx6_pcie->pci->dev,
+				"unable to enable pcie_ext_src clock\n");
+	}
+
 	switch (imx6_pcie->drvdata->variant) {
 	case IMX8QM:
 	case IMX8QM_EP:
@@ -770,6 +782,10 @@ static void imx6_pcie_clk_disable(struct imx6_pcie *imx6_pcie)
 	clk_disable_unprepare(imx6_pcie->pcie);
 	clk_disable_unprepare(imx6_pcie->pcie_bus);
 	clk_disable_unprepare(imx6_pcie->pcie_phy);
+	if (imx6_pcie->ext_src_clk_enabled) {
+		imx6_pcie->ext_src_clk_enabled = 0;
+		clk_disable_unprepare(imx6_pcie->pcie_ext_src);
+	}
 }
 
 static void imx6_pcie_assert_core_reset(struct imx6_pcie *imx6_pcie)
@@ -1577,6 +1593,16 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 	if (IS_ERR(imx6_pcie->pcie))
 		return dev_err_probe(dev, PTR_ERR(imx6_pcie->pcie),
 				     "pcie clock source missing or invalid\n");
+
+	imx6_pcie->pcie_ext_src = devm_clk_get(&pdev->dev,
+			"pcie_ext_src");
+	if (IS_ERR(imx6_pcie->pcie_ext_src)) {
+		if (PTR_ERR(imx6_pcie->pcie_ext_src) == -EPROBE_DEFER)
+			return PTR_ERR(imx6_pcie->pcie_ext_src);
+		imx6_pcie->pcie_ext_src = NULL;
+		dev_info(&pdev->dev,
+			"pcie_ext_src clk src missing or invalid\n");
+	}
 
 	switch (imx6_pcie->drvdata->variant) {
 	case IMX6SX:
