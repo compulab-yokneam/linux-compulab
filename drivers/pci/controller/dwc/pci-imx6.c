3a0f3aab1021d60bed926d7a15fa53cd05abf72e
Ilya Ledvich <ilya@compulab.co.il>
2020-06-04 14:59:36 +0300

    PCIe: imx6: release phy reset before toggling reset gpio
    Perform release release of PCIe PHY before device reset by
    toggling reset gpio.
    
    Signed-off-by: Ilya Ledvich <ilya@compulab.co.il>
    
    %% original patch: 0025-PCIe-imx6-release-phy-reset-before-toggling-reset-gp.patch

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -1182,15 +1182,6 @@ static void imx6_pcie_deassert_core_reset(struct imx6_pcie *imx6_pcie)
 		break;
 	}
 
-	/* Some boards don't have PCIe reset GPIO. */
-	if (gpio_is_valid(imx6_pcie->reset_gpio)) {
-		gpio_set_value_cansleep(imx6_pcie->reset_gpio,
-					imx6_pcie->gpio_active_high);
-		msleep(20);
-		gpio_set_value_cansleep(imx6_pcie->reset_gpio,
-					!imx6_pcie->gpio_active_high);
-	}
-
 	switch (imx6_pcie->drvdata->variant) {
 	case IMX8QM:
 	case IMX8QM_EP:
@@ -1345,6 +1336,15 @@ static void imx6_pcie_deassert_core_reset(struct imx6_pcie *imx6_pcie)
 	case IMX6Q:		/* Nothing to do */
 	case IMX6Q_EP:
 		break;
+	}
+
+	/* Some boards don't have PCIe reset GPIO. */
+	if (gpio_is_valid(imx6_pcie->reset_gpio)) {
+		gpio_set_value_cansleep(imx6_pcie->reset_gpio,
+					imx6_pcie->gpio_active_high);
+		msleep(100);
+		gpio_set_value_cansleep(imx6_pcie->reset_gpio,
+					!imx6_pcie->gpio_active_high);
 	}
 
 	return;
