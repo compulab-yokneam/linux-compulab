e47d1235bf100cb0df30e039cb33d2f966c44ca9
Valentin Raevsky <valentin@compulab.co.il>
2024-01-15 23:58:03 +0200

    ucm-imx8m-plus: dts: Update csi device tree files
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

---
+++ arch/arm64/boot/dts/compulab/overlay.h
@@ -0,0 +1,5 @@
+#include <dt-bindings/clock/imx8mp-clock.h>
+#include <dt-bindings/power/imx8mp-power.h>
+#include <dt-bindings/reset/imx8mp-reset.h>
+#include <dt-bindings/gpio/gpio.h>
+#include "../freescale/imx8mp-pinfunc.h"
