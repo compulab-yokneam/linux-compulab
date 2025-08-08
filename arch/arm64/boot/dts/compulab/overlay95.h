82e5ec4486507a338b62725d1df64f1318cf4ec6
Valentin Raevsky <valentin@compulab.co.il>
2025-08-08 12:24:30 +0300

    compulab: ucm-imx95: lvds: Enable touch screen
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- arch/arm64/boot/dts/compulab/overlay95.h
+++ arch/arm64/boot/dts/compulab/overlay95.h
@@ -3,5 +3,6 @@
 
 #include <dt-bindings/gpio/gpio.h>
 #include <dt-bindings/pwm/pwm.h>
+#include <dt-bindings/interrupt-controller/irq.h>
 #include "../freescale/imx95-pinfunc.h"
 #include "../freescale/imx95-clock.h"
