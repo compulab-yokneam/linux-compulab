3d747cf38a62007a5413e1731ed2c7669a499f1c
Ilya Ledvich <ilya@compulab.co.il>
2025-10-22 11:59:52 +0300

    net: phy: realtek: RTL8211F: add LED mode configuration via Device Tree
    Add support for the Device Tree property realtek,led-modes, which defines
    per-LED mode configuration.
    The property is a vector of zero to three 32-bit cells (u32), each
    in the range 0x0–0xf, where each cell sets the mode for one LED.
    Each cell’s value is a bitmap composed of the following bit flags:
     - 0b0001 - activity
     - 0b0010 - link10
     - 0b0100 - link100
     - 0b1000 - link1000
    Unset LEDs are omitted and will use their default configuration.
    
    Signed-off-by: Ilya Ledvich <ilya@compulab.co.il>

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -13,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <linux/clk.h>
+#include <dt-bindings/net/realtek-rtl8211x.h>
 
 #define RTL821x_PHYSR				0x11
 #define RTL821x_PHYSR_DUPLEX			BIT(13)
@@ -38,6 +39,14 @@
 #define RTL8211F_ALDPS_PLL_OFF			BIT(1)
 #define RTL8211F_ALDPS_ENABLE			BIT(2)
 #define RTL8211F_ALDPS_XTAL_OFF			BIT(12)
+
+#define RTL8211F_LCR				0x10
+#define RTL8211F_SPEED_10			BIT(0)
+#define RTL8211F_SPEED_100			BIT(1)
+#define RTL8211F_SPEED_1000			BIT(3)
+#define RTL8211F_ACTITITY			BIT(4)
+#define RTL8211F_LED_CTRL			5
+#define RTL8211F_LEDS_NUM			3
 
 #define RTL8211E_CTRL_DELAY			BIT(13)
 #define RTL8211E_TX_DELAY			BIT(12)
@@ -359,6 +368,46 @@ static int rtl8211x_config_init(struct phy_device *phydev)
 	return 0;
 };
 
+static int rtl8211f_config_leds(struct phy_device *phydev)
+{
+	struct device *dev = &phydev->mdio.dev;
+	u32 led_modes[RTL8211F_LEDS_NUM];
+	int len;
+	int ret=0;
+
+	len = of_property_read_variable_u32_array(dev->of_node, "realtek,led-modes",
+						  led_modes, 0, ARRAY_SIZE(led_modes));
+	if (len > 0) {
+		u16 reg = 0;
+		int i;
+
+		for (i = 0; i < len; i++) {
+			led_modes[i] &= PHY_LED_MASK;
+			/* RTL8211F: Active makes no sense if no LINK Bit set */
+			if (led_modes[i] == PHY_LED_ACTIVITY)
+				led_modes[i] = PHY_LED_LINK_ACTIVITY;
+
+			/* Parse the requested LED mode and set the corresponding bits in the control register */
+			reg |= (((led_modes[i] & PHY_LED_ACTIVITY)   ? (RTL8211F_ACTITITY)   : (0)) |
+				((led_modes[i] & PHY_LED_SPEED_10)   ? (RTL8211F_SPEED_10)   : (0)) |
+				((led_modes[i] & PHY_LED_SPEED_100)  ? (RTL8211F_SPEED_100)  : (0)) |
+				((led_modes[i] & PHY_LED_SPEED_1000) ? (RTL8211F_SPEED_1000) : (0))) << (i * RTL8211F_LED_CTRL);
+		}
+		dev_info(dev, "Applying LEDs configuration found in Device Tree\n");
+		dev_dbg(dev, "Set LCR (LED Control Register) value: 0x%04x\n", reg);
+		ret = phy_modify_paged_changed(phydev, 0xd04, RTL8211F_LCR, GENMASK(15,0), reg);
+		if (ret < 0) {
+			dev_err(dev, "Failed to update LED Control Register\n");
+			return ret;
+		}
+	} else {
+		dev_info(dev, "No valid LEDs configuration found, use defaults\n");
+		return -ENODATA;
+	}
+
+	return 0;
+}
+
 static int rtl8211f_config_init(struct phy_device *phydev)
 {
 	struct rtl821x_priv *priv = phydev->priv;
@@ -439,6 +488,13 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 			return ret;
 		}
 	}
+
+	ret = rtl8211f_config_leds(phydev);
+	if (ret < 0) {
+		dev_warn(dev, "Unable to apply LEDs settings from Device Tree, use defaults\n");
+	}
+	dev_dbg(dev, "Read LCR (LED Control Register): 0x%04x\n",
+		(unsigned int)phy_read_paged(phydev, 0xd04, RTL8211F_LCR));
 
 	return genphy_soft_reset(phydev);
 }
