93376b2984a3fa0d7ac255a558d4884886644a0f
Valentin Raevsky <valentin@compulab.co.il>
2026-05-21 20:59:33 +0000

    compulab: imx95: Update device tree files
    Merge from 6.12.34
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- arch/arm64/boot/dts/compulab/overlay95.h
+++ arch/arm64/boot/dts/compulab/overlay95.h
@@ -1,7 +1,11 @@
 /dts-v1/;
 /plugin/;
 
-#include <dt-bindings/clock/fsl,imx95-clock.h>
+#include <dt-bindings/clock/nxp,imx95-clock.h>
 #include <dt-bindings/gpio/gpio.h>
 #include <dt-bindings/pwm/pwm.h>
+#include <dt-bindings/dma/fsl-edma.h>
+#include <dt-bindings/input/input.h>
+#include <dt-bindings/interrupt-controller/arm-gic.h>
 #include "../freescale/imx95-pinfunc.h"
+#include "../freescale/imx95-clock.h"
