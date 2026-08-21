851cba624d9190eb2ce5857f6c72ae0a0a9a318f
Valentin Raevsky <valentin@compulab.co.il>
2026-08-21 11:10:49 +0000

    arm64: dts: compulab: add UCM-iMX952 peripheral overlays
    Add WM8962, RPMsg, AP1302, IMX219, and IMX477 overlays using the i.MX952 clock and camera topology.
    Register the standalone overlays and composite EVK targets in the UCM-iMX952 DT Makefile.
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- arch/arm64/boot/dts/compulab/overlay952.h
+++ arch/arm64/boot/dts/compulab/overlay952.h
@@ -1,6 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+
 /dts-v1/;
 /plugin/;
 
+#include <dt-bindings/clock/nxp,imx952-clock.h>
+#include <dt-bindings/dma/fsl-edma.h>
 #include <dt-bindings/gpio/gpio.h>
+#include <dt-bindings/input/input.h>
+#include <dt-bindings/interrupt-controller/arm-gic.h>
 #include <dt-bindings/pwm/pwm.h>
+#include "../freescale/imx952-clock.h"
 #include "../freescale/imx952-pinfunc.h"
